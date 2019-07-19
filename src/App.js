85c3db3c0c0edc063f4f7ceee6b33ef54ca40dae
Hector Flores <hflores@nearsoft.comm>
2019-07-19 13:04:50 -0700

    SVGR: component react js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>

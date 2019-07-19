b32a32f3ff33a2f6c32a14eec0919b681620d620
Hector Flores <hflores@nearsoft.comm>
2019-07-18 17:04:55 -0700

    Adding react and babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Mi proyecto con react avanzado!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<h1>Hola</h1>, app)
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,22 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }

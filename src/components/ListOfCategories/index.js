8beb7d31294eee36c3169c279824377dad3e03cd
Hector Flores <hflores@nearsoft.comm>
2019-07-19 15:54:27 -0700

    Cleaning events: useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 
 import { List, Item } from './styles'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [showFixed, setShowFixed] = useState(false)
+  const [categories, setCategories] = useState([])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  // componentDidMount
+  useEffect(function () {
+    window.fetch('https://petgram-server-hecto932.hflores.now.sh/categories')
+      .then(response => response.json())
+      .then(response => { setCategories(response) })
+  }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }

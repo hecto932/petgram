7985c34d8ca92d46fa8bf76399d4cd8b97c1c2d3
Hector Flores <hflores@nearsoft.comm>
2019-07-20 16:51:13 -0700

    Using render props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 
-const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={1} />
-  </div>
-)
+const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : (
+            <Fragment>
+              <ListOfCategories />
+              <ListOfPhotoCards categoryId={1} />
+            </Fragment>
+          )
+      }
+    </div>
+  )
+}
 
 export default App

84f9a8daa42d2ccd03f81e87350b9ec1f96435d3
Hector Flores <hflores@nearsoft.comm>
2019-07-20 14:32:57 -0700

    Graphql query params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
 
 const App = () => (
@@ -9,7 +9,7 @@ const App = () => (
     <GlobalStyle />
     <Logo />
     <ListOfCategories />
-    <ListOfPhotoCards />
+    <ListOfPhotoCards categoryId={1} />
   </div>
 )
 
--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,22 +1,7 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
 
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
   return (
     <ul>
       {
@@ -25,5 +10,3 @@ const ListOfPhotoCardsComponent = ({ data: { photos = [] } }) => {
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)

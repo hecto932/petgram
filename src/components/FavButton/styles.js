819e597ce51a2eaabe3544b40f6ffc233b710cf4
Hector Flores <hflores@nearsoft.comm>
2019-07-20 20:58:43 -0700

    Using mutations for likes

---
+++ src/components/FavButton/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  border: none;
+  display: flex;
+  align-items: center;
+  padding-top: 8px;
+
+  & svg {
+    margin-right: 4px;
+  }
+`
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,18 +1,17 @@
 import React, { Fragment } from 'react'
-import { Article, Button, ImgWrapper, Img } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Article, ImgWrapper, Img } from './styles'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  console.log(liked)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -23,10 +22,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} />
             </ImgWrapper>
           </a>
-
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size={`32px`} /> {likes} likes!
-          </Button>
+          <ToggleLikeMutation>
+            {(toggleLike) => {
+              const handleFavClick = () => {
+                !liked && toggleLike({ variables: { input: { id } } })
+                setLiked(!liked)
+              }
+              return (
+                <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+              )
+            }}
+          </ToggleLikeMutation>
         </Fragment>
       )}
     </Article>

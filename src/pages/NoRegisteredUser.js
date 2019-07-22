d539ca23f2af006553215d8b5461f453a9b7307e
Hector Flores <hflores@nearsoft.comm>
2019-07-21 18:43:49 -0700

    Login Mutation: Error and Loading

--- src/pages/NoRegisteredUser.js
+++ src/pages/NoRegisteredUser.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NoRegisteredUser = () => (
   <Context.Consumer>
@@ -17,11 +18,27 @@ export const NoRegisteredUser = () => (
                   .then(activateAuth)
               }
 
-              const errorMsg = error && 'El usuario ya no existe o hay algun problema.'
+              const errorMsg = error && 'El usuario ya existe o hay algun problema.'
               return <UserForm disabled={loading} title='Registrarse' onSubmit={onSubmit} error={errorMsg} />
             }}
           </RegisterMutation>
-          <UserForm title='Iniciar sesion' onSubmit={activateAuth} />
+
+          <LoginMutation >
+            {
+              (login, { data, loading, error }) => {
+                const onSubmit = ({ email, password }) => {
+                  const input = { email, password }
+                  const variables = { input }
+                  login({ variables })
+                    .then(activateAuth)
+                }
+
+                const errorMsg = error && 'Usuario o contrasña incorrecta...'
+                return <UserForm title='Iniciar sesion' onSubmit={onSubmit} disabled={loading} error={errorMsg} />
+              }
+            }
+          </LoginMutation>
+
         </Fragment>
       )
     }

9543015a6e5bdbd21a1d40a9da3b41e1a3e89357
Hector Flores <hflores@nearsoft.comm>
2019-07-21 23:03:54 -0700

    user likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img } from './styles'
 
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton'
@@ -10,10 +9,8 @@ import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -27,8 +24,7 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
           <ToggleLikeMutation>
             {(toggleLike) => {
               const handleFavClick = () => {
-                !liked && toggleLike({ variables: { input: { id } } })
-                setLiked(!liked)
+                toggleLike({ variables: { input: { id } } })
               }
               return (
                 <FavButton liked={liked} likes={likes} onClick={handleFavClick} />

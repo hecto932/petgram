655da706f3d1bc359b52284eab7377fa6a5ae0e2
Hector Flores <hflores@nearsoft.comm>
2019-07-23 10:13:11 -0700

    React.lazy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { NoRegisteredUser } from './pages/NoRegisteredUser'
 import { User } from './pages/User'
 import { NavBar } from './components/NavBar'
@@ -13,10 +13,12 @@ import { NotFound } from './pages/NotFound'
 import { Router, Redirect } from '@reach/router'
 import { Context } from './Context'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <div>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -32,7 +34,7 @@ const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </div>
+    </Suspense>
   )
 }
 
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Layout } from '../components/Layout'
-import { Helmet } from 'react-helmet'
 import { FavsWithQuery } from '../container/GetFavorites'
 
-export const Favs = () => (
+export default () => (
   <Layout title='Tus favoritos' subtitle='Aqui puedes encontrar tus favoritos'>
     <FavsWithQuery />
   </Layout>

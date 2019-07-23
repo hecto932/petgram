c1f3e687affaa440a76ef065ed8eb8f3ffe41248
Hector Flores <hflores@nearsoft.comm>
2019-07-23 10:04:18 -0700

    React.memo

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -22,7 +22,7 @@ function useCategoriesData () {
   return { categories, loading }
 }
 
-export const ListOfCategories = () => {
+const ListOfCategoriesComponent = () => {
   const { categories, loading } = useCategoriesData()
   const [showFixed, setShowFixed] = useState(false)
 
@@ -54,3 +54,5 @@ export const ListOfCategories = () => {
     </Fragment>
   )
 }
+
+export const ListOfCategories = React.memo(ListOfCategoriesComponent)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,13 @@ import { Layout } from '../components/Layout'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 
-export const Home = ({ id }) => (
+const HomePage = ({ categoryId }) => (
   <Layout title='Tu app de fotos de mascotas' subtitle='Con petgram podemos encontrar fotos de animales demesticos muy bonitos'>
     <ListOfCategories />
-    <ListOfPhotoCards categoryId={id} />
+    <ListOfPhotoCards categoryId={categoryId} />
   </Layout>
 )
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.categoryId === props.categoryId
+})

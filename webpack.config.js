3b29c8f7a7794f44a42475e7e552075618ced88c
Hector Flores <Hector_Flores@eogresources.com>
2019-07-24 15:24:18 -0700

    Generating manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const WebpackPwaManifest = require('webpack-pwa-manifest')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
@@ -11,6 +12,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new WebpackPwaManifest({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      short_name: 'Petgram',
+      description: 'Con petgram puedes encontrar perros bonitos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          size: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ],
   module: {
